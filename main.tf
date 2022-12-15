2f7b67d57675ba914f24424cda40950365e1571e
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2022-12-15 21:49:53 +0530

    jd

--- main.tf
+++ main.tf
@@ -4,14 +4,6 @@ access_key = "AKIA4QUKASHDSEKLAZKU"
 secret_key = "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"
 }
 
-resource "aws_instance" "one" {
-ami = "ami-0b0dcb5067f052a63"
-instance_type = "t2.micro"
-tags = {
-Name = "Terrainstance"
-}
-}
-
 resource "aws_s3_bucket" "two" {
   bucket = "raham887799"
 }
